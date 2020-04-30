606e252bef1d2fe12d021d0033dbbc06bcf46dac
mays <mrubaye@terpmail.umd.edu>
2020-04-30 15:18:58 -0400

    updated nav

--- utcTechSources/settings/base.py
+++ utcTechSources/settings/base.py
@@ -150,6 +150,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
+
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/2.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
